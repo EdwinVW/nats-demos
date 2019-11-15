511a6cf50ad27856b0ac4a58a9ed81262bf2ab89
Edwin van Wijk <edwinvw@gmail.com>
2019-11-15 09:36:34 +0100

    Updated monitoring script

--- src/NATS-Streaming/Store/start-monitor.ps1
+++ src/NATS-Streaming/Store/start-monitor.ps1
@@ -2,4 +2,6 @@
 # npm install natsboard -g
 
 Start-Process -FilePath "natsboard" -ArgumentList "--nats-mon-url http://localhost:8223"
+Start-Process -FilePath "http://localhost:8223"
+Start-Process -FilePath "http://localhost:8223/streaming"
 Start-Process -FilePath "http://localhost:3000"
